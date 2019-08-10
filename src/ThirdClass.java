25ca25b3fba17b848079c68ff4c9e11e8e7c14ed
qa-eng <qa.eng.qa@gmail.com>
2019-08-10 12:29:36 +0300

    Third class new line added

--- src/ThirdClass.java
+++ src/ThirdClass.java
@@ -2,5 +2,6 @@ public class ThirdClass {
     public static void main(String[] args) {
         System.out.println("Third class!");
         System.out.println("Third class!");
+        System.out.println("Third class!");
     }
 }
