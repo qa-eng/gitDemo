3e65698b2ccbcb476313ff1724c3ceac41aef777
qa-eng <qa.eng.qa@gmail.com>
2019-08-10 12:24:57 +0300

    Second class final version

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -2,5 +2,6 @@ public class SecondClass {
     public static void main(String[]args){
         System.out.println("Second class!");
         System.out.println("Second class!");
+        System.out.println("Second class!");
     }
 }
